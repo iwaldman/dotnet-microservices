c759b7bf4283f97908a49fc412697dfe891b49e8
Irvin Waldman <irvin.waldman@hotmail.com>
2021-10-13 20:38:52 -0500

    Platform controller methods

--- PlatformService/Controllers/PlatformsController.cs
+++ PlatformService/Controllers/PlatformsController.cs
@@ -5,6 +5,8 @@ using System.Threading.Tasks;
 using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using PlatformService.Data;
+using PlatformService.Dtos;
+using PlatformService.Models;
 
 namespace PlatformService.Controllers
 {
@@ -14,11 +16,50 @@ namespace PlatformService.Controllers
     {
         private readonly IPlatformRepo _repo;
         private readonly IMapper _mapper;
-
         public PlatformsController(IPlatformRepo repo, IMapper mapper)
         {
             _mapper = mapper;
             _repo = repo;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<PlatformReadDto>> GetPlatforms()
+        {
+            var platformItems = _repo.GetAllPlatforms();
+
+            var results = _mapper.Map<IEnumerable<PlatformReadDto>>(platformItems);
+
+            return Ok(results);
+        }
+
+        [HttpGet("{id}", Name = "GetPlatformById")]
+        public ActionResult<PlatformReadDto> GetPlatformById(int id)
+        {
+            var platformItem = _repo.GetPlatformById(id);
+
+            if (platformItem is null)
+            {
+                return NotFound();
+            }
+
+            var result = _mapper.Map<PlatformReadDto>(platformItem);
+
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public ActionResult<PlatformReadDto> CreatePlatform(PlatformCreateDto createDto)
+        {
+            var itemToCreate = _mapper.Map<Platform>(createDto);
+            _repo.CreatePlatform(itemToCreate);
+            _repo.SaveChanges();
+
+            var platformReadDto = _mapper.Map<PlatformReadDto>(itemToCreate);
+
+            return CreatedAtRoute(
+                nameof(GetPlatformById),
+                new { Id = platformReadDto.Id },
+                platformReadDto);
         }
     }
 }
