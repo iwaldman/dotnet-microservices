cea27a7a610b053327cf81c414da61f92322c48e
Irvin Waldman <iwaldman@pros.com>
2021-10-29 16:49:01 -0500

    Add CommandsService

---
+++ CommandsService/Controllers/PlatformsController.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CommandsService.Controllers
+{
+    [ApiController]
+    [Route("api/c/[controller]")]
+    public class PlatformsController : ControllerBase
+    {
+        public PlatformsController()
+        {
+
+        }
+
+        [HttpPost]
+        public ActionResult TestInboundConnection()
+        {
+            Console.WriteLine("Inbound POST # Commands Service");
+
+            return Ok("Inbound POST # Commands Service");
+        }
+    }
+}
