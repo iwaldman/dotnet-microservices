09b98f003e653e522d7192b8be56fd719772cd70
Irvin M. Waldman <irvin.waldman@hotmail.com>
2021-11-01 21:55:02 -0500

    DI HttpCommandDataClient

--- PlatformService/Controllers/PlatformsController.cs
+++ PlatformService/Controllers/PlatformsController.cs
@@ -1,9 +1,11 @@
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using PlatformService.Data;
 using PlatformService.Dtos;
 using PlatformService.Models;
+using PlatformService.SyncDataServices.Http;
 
 namespace PlatformService.Controllers
 {
@@ -13,8 +15,14 @@ namespace PlatformService.Controllers
     {
         private readonly IPlatformRepo _repo;
         private readonly IMapper _mapper;
-        public PlatformsController(IPlatformRepo repo, IMapper mapper)
+        private readonly ICommandDataClient _commandDataClient;
+
+        public PlatformsController(
+            IPlatformRepo repo,
+            IMapper mapper,
+            ICommandDataClient commandDataClient)
         {
+            _commandDataClient = commandDataClient;
             _mapper = mapper;
             _repo = repo;
         }
@@ -45,13 +53,23 @@ namespace PlatformService.Controllers
         }
 
         [HttpPost]
-        public ActionResult<PlatformReadDto> CreatePlatform(PlatformCreateDto platformCreateDto)
+        public async Task<ActionResult<PlatformReadDto>> CreatePlatform(
+            PlatformCreateDto platformCreateDto)
         {
             var platformModel = _mapper.Map<Platform>(platformCreateDto);
             _repo.CreatePlatform(platformModel);
             _repo.SaveChanges();
 
             var platformReadDto = _mapper.Map<PlatformReadDto>(platformModel);
+
+            try
+            {
+                await _commandDataClient.SendPlatformToCommand(platformReadDto);
+            }
+            catch (System.Exception)
+            {
+                throw;
+            }
 
             return CreatedAtRoute(
                 nameof(GetPlatformById),
